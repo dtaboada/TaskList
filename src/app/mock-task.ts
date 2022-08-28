84e8a562eff3b4cbea51f1a0a12b17ddebdbbb79
Diego Taboada <dsp_diego@hotmail.com>
2022-08-28 16:21:44 -0300

    Proyecto completo + ng + jsonServer

---
+++ src/app/mock-task.ts
@@ -0,0 +1,32 @@
+import  {Task} from "./Task"
+
+export const TASKS: Task[] = [
+    {
+        id:1,
+        text: "Terminar Algo Loco",
+        day: '12 novienbre 1984',
+        reminder: true    
+    },
+
+    {
+        id:2,
+        text: "Hacer compras",
+        day: '05 Marzo 2014',
+        reminder: true    
+    },
+
+    {
+        id:3,
+        text: "Estudiar AP",
+        day: '12 Julio 1982',
+        reminder: false    
+    },
+
+    {
+        id:4,
+        text: "Leer Libro",
+        day: '02 Enero 2022',
+        reminder: false    
+    },
+
+]
